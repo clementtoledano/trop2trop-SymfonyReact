923ddad8791737e818899b73a0f7486fef57642f
clement.toledano <clement.toledano@gmail.com>
2020-05-26 16:28:49 +0200

    test ovh

--- src/Migrations/Version20200526142806.php
+++ src/Migrations/Version20200526142806.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200503093604 extends AbstractMigration
+final class Version20200526142806 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -24,15 +24,13 @@ final class Version20200503093604 extends AbstractMigration
 
         $this->addSql('CREATE TABLE hashtag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE media_object (id INT AUTO_INCREMENT NOT NULL, file_path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, post_id INT NOT NULL, name VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_C53D045F4B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, image_id INT NOT NULL, user_id INT NOT NULL, content LONGTEXT NOT NULL, is_active TINYINT(1) NOT NULL, create_at DATETIME NOT NULL, update_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_5A8A6C8D3DA5256D (image_id), INDEX IDX_5A8A6C8DA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_feelingSilly (post_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_DAF9CF5A4B89032C (post_id), INDEX IDX_DAF9CF5AA76ED395 (user_id), PRIMARY KEY(post_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_feelingScary (post_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_69DD83B24B89032C (post_id), INDEX IDX_69DD83B2A76ED395 (user_id), PRIMARY KEY(post_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_feelingAngry (post_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_851AE33F4B89032C (post_id), INDEX IDX_851AE33FA76ED395 (user_id), PRIMARY KEY(post_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_feelingBored (post_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_66E775E4B89032C (post_id), INDEX IDX_66E775EA76ED395 (user_id), PRIMARY KEY(post_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE post_hashtag (post_id INT NOT NULL, hashtag_id INT NOT NULL, INDEX IDX_675D9D524B89032C (post_id), INDEX IDX_675D9D52FB34EF56 (hashtag_id), PRIMARY KEY(post_id, hashtag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, image_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, is_admin TINYINT(1) NOT NULL, create_at DATETIME NOT NULL, update_at DATETIME DEFAULT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D6493DA5256D (image_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045F4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles TINYTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, create_at DATETIME NOT NULL, update_at DATETIME DEFAULT NULL, password_change_date INT DEFAULT NULL, confirmation_token VARCHAR(40) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D3DA5256D FOREIGN KEY (image_id) REFERENCES media_object (id)');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE users_feelingSilly ADD CONSTRAINT FK_DAF9CF5A4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
@@ -45,7 +43,6 @@ final class Version20200503093604 extends AbstractMigration
         $this->addSql('ALTER TABLE users_feelingBored ADD CONSTRAINT FK_66E775EA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE post_hashtag ADD CONSTRAINT FK_675D9D524B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
         $this->addSql('ALTER TABLE post_hashtag ADD CONSTRAINT FK_675D9D52FB34EF56 FOREIGN KEY (hashtag_id) REFERENCES hashtag (id)');
-        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6493DA5256D FOREIGN KEY (image_id) REFERENCES media_object (id)');
     }
 
     public function down(Schema $schema) : void
@@ -55,8 +52,6 @@ final class Version20200503093604 extends AbstractMigration
 
         $this->addSql('ALTER TABLE post_hashtag DROP FOREIGN KEY FK_675D9D52FB34EF56');
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D3DA5256D');
-        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6493DA5256D');
-        $this->addSql('ALTER TABLE image DROP FOREIGN KEY FK_C53D045F4B89032C');
         $this->addSql('ALTER TABLE users_feelingSilly DROP FOREIGN KEY FK_DAF9CF5A4B89032C');
         $this->addSql('ALTER TABLE users_feelingScary DROP FOREIGN KEY FK_69DD83B24B89032C');
         $this->addSql('ALTER TABLE users_feelingAngry DROP FOREIGN KEY FK_851AE33F4B89032C');
@@ -69,7 +64,6 @@ final class Version20200503093604 extends AbstractMigration
         $this->addSql('ALTER TABLE users_feelingBored DROP FOREIGN KEY FK_66E775EA76ED395');
         $this->addSql('DROP TABLE hashtag');
         $this->addSql('DROP TABLE media_object');
-        $this->addSql('DROP TABLE image');
         $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE users_feelingSilly');
         $this->addSql('DROP TABLE users_feelingScary');
